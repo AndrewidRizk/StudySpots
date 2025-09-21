b4ada86f68af87b5b94fb6fd19ecaf030ab33676
Andrewid <Androwmaged47@gmail.com>
2025-09-21 16:59:30 -0400

    Update _app.tsx

--- StudySpot/client/src/app/_app.tsx
+++ StudySpot/client/src/app/_app.tsx
@@ -1,14 +1,25 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useEffect } from "react";
+import * as FullStory from "@fullstory/browser";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Only init FullStory in production
+    if (process.env.NODE_ENV === "production" && process.env.NEXT_PUBLIC_FS_ORG_ID) {
+      FullStory.init({ orgId: process.env.NEXT_PUBLIC_FS_ORG_ID });
+    }
+  }, []);
+
   return (
     <>
       <Head>
-        <title>{process.env.APP_NAME || "Default App Name"}</title>
+        <title>{process.env.NEXT_PUBLIC_APP_NAME || "Default App Name"}</title>
         <meta
           name="description"
-          content={process.env.APP_DESCRIPTION || "Default App Description"}
+          content={
+            process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Default App Description"
+          }
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
